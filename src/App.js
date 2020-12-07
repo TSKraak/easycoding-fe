7c38b839ccbcc6c73b063cf3ea3e83e5bdfefed6
TSKraak <tskraak@gmail.com>
2020-12-07 12:33:34 +0100

    Basic setup with React & Redux and user store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+
+const NotFound = () => {
+  return <h3>Oops, sorry. Page doesn't exist.</h3>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/home">
+          <HomePage />
+        </Route>
+        {/* <Route exact path="/posts/:language?">
+<PostsPage />
+</Route> */}
+        <Route exact path="/" component={HomePage} />
+        <Route path="/" component={NotFound} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
---
+++ src/store/user/actions.js
@@ -0,0 +1,124 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+import { selectToken } from "./selectors";
+import {
+  appLoading,
+  appDoneLoading,
+  showMessageWithTimeout,
+  setMessage,
+} from "../appState/actions";
+
+const loginSuccess = (userWithToken) => {
+  return {
+    type: "LOGIN_SUCCESS",
+    payload: userWithToken,
+  };
+};
+
+const tokenStillValid = (userWithoutToken) => ({
+  type: "TOKEN_STILL_VALID",
+  payload: userWithoutToken,
+});
+
+export const logOut = () => ({ type: "LOG_OUT" });
+
+export const signUp = (
+  firstName,
+  lastName,
+  city,
+  country,
+  imageUrl,
+  email,
+  password
+) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await axios.post(`${apiUrl}/signup`, {
+        firstName,
+        lastName,
+        city,
+        country,
+        imageUrl,
+        email,
+        password,
+      });
+
+      dispatch(loginSuccess(response.data));
+      dispatch(showMessageWithTimeout("success", true, "Account created"));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage("danger", true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
+
+export const login = (email, password) => {
+  return async (dispatch, getState) => {
+    dispatch(appLoading());
+    try {
+      const response = await axios.post(`${apiUrl}/login`, {
+        email,
+        password,
+      });
+      // console.log("RESPONSE DATA:", response.data);
+      if (response.data.accountBlocked) {
+        dispatch(setMessage("danger", true, "Your account has been blocked"));
+        dispatch(logOut());
+        return dispatch(appDoneLoading());
+      }
+      dispatch(loginSuccess(response.data));
+      dispatch(showMessageWithTimeout("success", false, "Welcome back!", 1500));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data.message);
+        dispatch(setMessage("danger", true, error.response.data.message));
+      } else {
+        console.log(error.message);
+        dispatch(setMessage("danger", true, error.message));
+      }
+      dispatch(appDoneLoading());
+    }
+  };
+};
+
+export const getUserWithStoredToken = () => {
+  return async (dispatch, getState) => {
+    // get token from the state
+    const token = selectToken(getState());
+
+    // if we have no token, stop
+    if (!token) return;
+
+    dispatch(appLoading());
+    try {
+      // if we do have a token,
+      // check wether it is still valid or if it is expired
+      const response = await axios.get(`${apiUrl}/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      // token is still valid
+      dispatch(tokenStillValid(response.data));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.message);
+      } else {
+        console.log(error);
+      }
+      // if we get a 4xx or 5xx response,
+      // get rid of the token by logging out
+      dispatch(logOut());
+      dispatch(appDoneLoading());
+    }
+  };
+};
