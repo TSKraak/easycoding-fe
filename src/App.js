2f7b759572b1934367f73cda2ca0b11b5ba739f7
TSKraak <tskraak@gmail.com>
2020-12-07 17:49:49 +0100

    Added a basic footer to the bottom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Navigation from "./components/Navigation";
 import Loading from "./components/Loading";
 import MessageBox from "./components/MessageBox";
@@ -15,6 +15,7 @@ import { selectAppLoading } from "./store/appState/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getUserWithStoredToken } from "./store/user/actions";
+import Footer from "./components/Footer";
 
 const NotFound = () => {
   return <h3>Oops, sorry. Page doesn't exist.</h3>;
@@ -33,43 +34,45 @@ function App() {
       <Navigation />
       {isLoading ? <Loading /> : null}
       <MessageBox />
+      <div className="App-Content">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/posts">
+            <PostPage />
+          </Route>
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/posts">
-          <PostPage />
-        </Route>
+          <Route exact path="/posts/:language?">
+            <PostPage />
+          </Route>
 
-        <Route exact path="/posts/:language?">
-          <PostPage />
-        </Route>
+          <Route exact path="/requests">
+            <RequestPage />
+          </Route>
 
-        <Route exact path="/requests">
-          <RequestPage />
-        </Route>
+          <Route exact path="/favourites">
+            <FavouritePage />
+          </Route>
 
-        <Route exact path="/favourites">
-          <FavouritePage />
-        </Route>
+          <Route exact path="/admin/manage-users">
+            <ManageUsersPage />
+          </Route>
 
-        <Route exact path="/admin/manage-users">
-          <ManageUsersPage />
-        </Route>
+          <Route exact path="/profile">
+            <ProfilePage />
+          </Route>
 
-        <Route exact path="/profile">
-          <ProfilePage />
-        </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
 
-        <Route exact path="/login">
-          <Login />
-        </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
 
-        <Route exact path="/signup">
-          <SignUp />
-        </Route>
-
-        <Route path="/" component={NotFound} />
-      </Switch>
+          <Route path="/" component={NotFound} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomePage() {
   return (
     <div>
-      <h1>Homepage..</h1>
+      <h1>Welcome to &#60;Easy Coding&#62;</h1>
     </div>
   );
 }
