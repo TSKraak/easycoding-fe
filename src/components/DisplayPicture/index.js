dd9135a3c715014c478e0f9725a37ea464f7d698
ariqyandri <ariq140199@gmail.com>
2020-12-08 15:49:13 +0100

    Create display picture component

---
+++ src/components/DisplayPicture/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { selectAppLoading } from "../../store/appState/selectors";
+import Loading from "../Loading";
+import "./DisplayImage.css";
+
+export default function DisplayPicture(props) {
+  const loading = useSelector(selectAppLoading);
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <Carousel className="displayImageCarousel">
+        {props.picture.map((pic) => {
+          return (
+            <Carousel.Item key={pic.id} interval={10000}>
+              <div className="displayImageBoxOver">
+                <div className="displayImageBox">
+                  <img
+                    className="d-block displayImage"
+                    src={pic.url}
+                    alt={pic.name}
+                  />
+                </div>
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+}
