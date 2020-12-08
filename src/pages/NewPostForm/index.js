1679d6454b8a11bd6b1c73bf129dc595ce57915b
ariqyandri <ariq140199@gmail.com>
2020-12-08 15:38:39 +0100

    Add validation and update sizing new post form

--- src/pages/NewPostForm/index.js
+++ src/pages/NewPostForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Col, Container, Form } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import ImagePreview from "../../components/PreviewPicture";
@@ -9,22 +9,36 @@ import { createPost } from "../../store/post/actions";
 export default function NewPostForm() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [validated, setValidated] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
 
   async function submitForm(event) {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    event.preventDefault();
+    setValidated(true);
     event.preventDefault();
 
-    await dispatch(createPost(title, content));
-
-    setTitle("");
-    setContent("");
-
-    history.push("/posts");
+    if (title !== "" || content !== "") {
+      await dispatch(createPost(title, content));
+      setTitle("");
+      setContent("");
+      history.push("/posts");
+    }
   }
   return (
     <Container>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+      <Form
+        md={{ span: 6, offset: 3 }}
+        className="mt-5"
+        noValidate
+        validated={validated}
+        onSubmit={submitForm}
+      >
         <h1 className="mt-5 mb-5">Create Post</h1>
         <Form.Group controlId="formPostTitle">
           <Form.Label>Post Title</Form.Label>
@@ -35,22 +49,29 @@ export default function NewPostForm() {
             placeholder="Enter title"
             required
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a title.
+          </Form.Control.Feedback>
         </Form.Group>
-
         <Form.Group controlId="formPostText">
           <Form.Label>Post Text</Form.Label>
           <Form.Control
             value={content}
             onChange={(event) => setContent(event.target.value)}
             type="text"
+            as="textarea"
+            rows={15}
             placeholder="Enter content"
             required
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a text.
+          </Form.Control.Feedback>
         </Form.Group>
         <ImagePreview />
         <UploadPostPicture />
         <Form.Group className="mt-5">
-          <Button variant="primary" type="submit" onClick={submitForm}>
+          <Button variant="primary" type="submit">
             Create Post
           </Button>
         </Form.Group>
