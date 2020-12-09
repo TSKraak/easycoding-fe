535c831ce406b037cc01653433dc6354371968bb
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-09 11:22:50 +0100

    Connect form with useState and Submit form

--- src/pages/NewRequestForm/index.js
+++ src/pages/NewRequestForm/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 export default function NewRequestForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [validated, setValidated] = useState(false);
+  const history = useHistory();
+
+  async function submitForm(event) {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    event.preventDefault();
+    setValidated(true);
+    event.preventDefault();
+
+    if (title !== "" && content !== "") {
+      //   await dispatch(createPost(title, content));
+      setTitle("");
+      setContent("");
+      history.push("/requests");
+    }
+  }
   return (
     <div>
       <h1>Create Request</h1>
@@ -9,14 +32,14 @@ export default function NewRequestForm() {
         <Form
           md={{ span: 6, offset: 3 }}
           noValidate
-          //   validated={validated}
-          //   onSubmit={submitForm}
+          validated={validated}
+          onSubmit={submitForm}
         >
           <Form.Group controlId="formRequestTitle">
             <Form.Label>Request Title</Form.Label>
             <Form.Control
-              //   value={title}
-              //   onChange={(event) => setTitle(event.target.value)}
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
               type="title"
               placeholder="Enter title"
               required
@@ -28,8 +51,8 @@ export default function NewRequestForm() {
           <Form.Group controlId="formRequestText">
             <Form.Label>Request Text</Form.Label>
             <Form.Control
-              //   value={content}
-              //   onChange={(event) => setContent(event.target.value)}
+              value={content}
+              onChange={(event) => setContent(event.target.value)}
               type="text"
               as="textarea"
               rows={2}
