d2e166c05edc6cd3c1729d8f0580b3df3f440581
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-09 14:43:41 +0100

    Show message if no result and add Thomas's comment section

--- src/pages/RequestPage/index.js
+++ src/pages/RequestPage/index.js
@@ -4,9 +4,10 @@ import Loading from "../../components/Loading";
 import { selectAppLoading } from "../../store/appState/selectors";
 import { fetchRequests } from "../../store/request/actions";
 import { selectRequests } from "../../store/request/selectors";
-import { Button, Card, Form, FormControl } from "react-bootstrap";
+import { Accordion, Button, Card, Form, FormControl } from "react-bootstrap";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import Comments from "../../components/Comments";
 
 export default function RequestPage() {
   const [searchText, setSearchText] = useState("");
@@ -64,35 +65,91 @@ export default function RequestPage() {
           <Button variant="success">Create New Request</Button>
         </Link>
       </Form>
-      {loading || !searchResult
-        ? requests.map((req) => {
-            return (
-              <Card key={req.id} style={{ margin: "1rem", width: "30rem" }}>
-                <Card.Header>{req.title}</Card.Header>
-                <Card.Body>
-                  <Card.Text>{req.content}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  requested by {req.user.name}{" "}
-                  {moment(req.createdAt).format("DD/MM/YYYY")}
-                </Card.Footer>
-              </Card>
-            );
-          })
-        : searchResult.map((req) => {
-            return (
-              <Card key={req.id} style={{ margin: "1rem", width: "30rem" }}>
-                <Card.Header>{req.title}</Card.Header>
-                <Card.Body>
-                  <Card.Text>{req.content}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  requested by {req.user.name}{" "}
-                  {moment(req.createdAt).format("DD/MM/YYYY")}
-                </Card.Footer>
-              </Card>
-            );
-          })}
+      {loading || !searchResult ? (
+        requests.map((req) => {
+          return (
+            <Card key={req.id} style={{ margin: "1rem", width: "30rem" }}>
+              <Card.Header>{req.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{req.content}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                requested by {req.user.name}{" "}
+                {moment(req.createdAt).format("DD/MM/YYYY")}
+              </Card.Footer>
+              <Accordion>
+                <Card
+                  bg="light"
+                  style={{
+                    width: "58rem",
+                    marginLeft: "2rem",
+                  }}
+                  className="mb-2"
+                >
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    style={{
+                      background: "lightgrey",
+                      fontSize: "0.9rem",
+                      margin: "0",
+                      padding: "0.5rem",
+                    }}
+                    eventKey="0"
+                  >
+                    View comments (click here)
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Comments requestId={req.id} commentType="request" />
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Card>
+          );
+        })
+      ) : !searchResult.length ? (
+        <h4>No search results..</h4>
+      ) : (
+        searchResult.map((req) => {
+          return (
+            <Card key={req.id} style={{ margin: "1rem", width: "30rem" }}>
+              <Card.Header>{req.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{req.content}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                requested by {req.user.name}{" "}
+                {moment(req.createdAt).format("DD/MM/YYYY")}
+              </Card.Footer>
+              <Accordion>
+                <Card
+                  bg="light"
+                  style={{
+                    width: "58rem",
+                    marginLeft: "2rem",
+                  }}
+                  className="mb-2"
+                >
+                  <Accordion.Toggle
+                    as={Card.Header}
+                    style={{
+                      background: "lightgrey",
+                      fontSize: "0.9rem",
+                      margin: "0",
+                      padding: "0.5rem",
+                    }}
+                    eventKey="0"
+                  >
+                    View comments (click here)
+                  </Accordion.Toggle>
+                  <Accordion.Collapse eventKey="0">
+                    <Comments requestId={req.id} commentType="request" />
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </Card>
+          );
+        })
+      )}
     </div>
   );
 }
