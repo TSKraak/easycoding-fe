550ca0b962e6c9920664250aa1e9f93f463520a5
JANNIS-PASSALIS <PASALIS13@HOTMAIL.COM>
2020-12-09 15:38:14 +0100

    Make searchbar in post page functional

--- src/pages/PostPage/index.js
+++ src/pages/PostPage/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../../store/post/actions";
 import { selectAllPosts } from "../../store/post/selectors";
 import { Button, Card, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FavouriteButton from "../../components/FavouriteButton";
+import Loading from "../../components/Loading";
 
 export default function PostPage() {
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState("");
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
   console.log("what is posts", posts);
@@ -14,6 +17,24 @@ export default function PostPage() {
   useEffect(() => {
     dispatch(fetchPosts);
   }, [dispatch]);
+
+  async function submitForm(event) {
+    event.preventDefault();
+
+    setSearchResult(
+      posts.filter((post) => {
+        if (post.content.indexOf(searchText) !== -1) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    );
+
+    setSearchText("");
+  }
+
+  console.log("what is searchResults", searchResult);
 
   return (
     <div>
@@ -28,11 +49,15 @@ export default function PostPage() {
         }}
       >
         <FormControl
+          value={searchText}
+          onChange={(event) => setSearchText(event.target.value)}
           type="text"
           placeholder="Search For Posts"
           className="mr-sm-2"
         />
-        <Button variant="outline-primary">Search</Button>
+        <Button variant="outline-primary" onClick={submitForm}>
+          Search
+        </Button>
         <p style={{ margin: "20px" }}>or</p>
         <Link to="/posts/new">
           <Button variant="success">Create New Post</Button>
@@ -47,24 +72,45 @@ export default function PostPage() {
           justifyContent: "flex-start",
         }}
       >
-        {!posts
-          ? "...Loading"
-          : posts.map((post) => {
-              return (
-                <Card key={post.id} style={{ margin: "1rem", width: "20rem" }}>
-                  <Card.Body>
-                    <Card.Title>{post.title}</Card.Title>
-                    <Link to={`/posts/details/${post.id}`}>
-                      <Button variant="outline-primary">
-                        View Details of {post.title}
-                      </Button>
-                    </Link>
-                    <FavouriteButton postId={post.id} />{" "}
-                  </Card.Body>
-                  <Card.Footer>written by {post.author.name}</Card.Footer>
-                </Card>
-              );
-            })}
+        {!posts ? (
+          <Loading />
+        ) : !searchResult ? (
+          posts.map((post) => {
+            return (
+              <Card key={post.id} style={{ margin: "1rem", width: "20rem" }}>
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Link to={`/posts/details/${post.id}`}>
+                    <Button variant="outline-primary">
+                      View Details of {post.title}
+                    </Button>
+                  </Link>
+                  <FavouriteButton postId={post.id} />{" "}
+                </Card.Body>
+                <Card.Footer>written by {post.author.name}</Card.Footer>
+              </Card>
+            );
+          })
+        ) : !searchResult.length ? (
+          <h3>No search results</h3>
+        ) : (
+          searchResult.map((post) => {
+            return (
+              <Card key={post.id} style={{ margin: "1rem", width: "20rem" }}>
+                <Card.Body>
+                  <Card.Title>{post.title}</Card.Title>
+                  <Link to={`/posts/details/${post.id}`}>
+                    <Button variant="outline-primary">
+                      View Details of {post.title}
+                    </Button>
+                  </Link>
+                  <FavouriteButton postId={post.id} />{" "}
+                </Card.Body>
+                <Card.Footer>written by {post.author.name}</Card.Footer>
+              </Card>
+            );
+          })
+        )}
       </div>
     </div>
   );
